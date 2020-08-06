488baa471d70654e655ba7802927c22f87af851f
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-08-06 17:38:50 +0200

    totally different

--- vapor/__init__.py
+++ vapor/__init__.py
@@ -1,76 +1,157 @@
 import os
 import argparse
-from multiprocessing import Pool
+from multiprocessing import Pool, Queue
 from pysam import VariantFile
 import tempfile
 import sys
 import subprocess
 import time
+import re
 
 from functools import partial
+from itertools import chain, zip_longest , islice
+
+import multiprocessing as mp
+from threading import Thread
+
+# ctx = mp.get_context()
+
+# class VaporProcess(ctx.Process):
+#     def __init__(self, command, header, queue, *args, **kwargs):
+#         print("start VaporProcess", file=sys.stderr)
+#         cmd = f"{command}"
+#         self.sp = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE)
+#         self.queue = queue
+#         super().__init__(*args, **kwargs)
+
+# ctx.Process = VaporProcess
+
+def execute_single():
+    command = execute_single.command
+    inqueue = execute_single.inqueue
+    outqueue = execute_single.outqueue
+
+    sp = subprocess.Popen(f"{command}", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)    
+    inpipe = sp.stdin
+    outpipe = sp.stdout
+
+    inpipe.write(execute_single.header.encode())
+
+    def write():
+        while True:
+            records = inqueue.get()
+            if records is None: # stop signal for write
+                print("send close signal", file=sys.stderr)
+                inpipe.close()
+                break
+            for record in records:
+                inpipe.write(record.encode())
+
+    def read():
+        for line in outpipe:
+            if line.startswith(b"#"):
+                continue
+            outqueue.put(line.decode())
+
+    writer = Thread(target=write)
+    writer.start()
+    reader = Thread(target=read)
+    reader.start()
+    writer.join()
+    print("writer closed", file=sys.stderr)
+    reader.join()
+    print("reader closed", file=sys.stderr)
+    print("worker closed", file=sys.stderr)
 
 
-def execute_single(filename, command, tmp_dir, region):
-    # generate outfilename
-    outfilename = os.path.join(tmp_dir, f"{'_'.join(map(str, region))}.vcf")
-    cmd = f"{command} > {outfilename}"
-    sp = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE)
-    pipe = sp.stdin
+def grouper(n, records, bnd_reg, mate_id_reg):
+    bnds = dict()
+    ret = []
+    for i, record in enumerate(records):
+        if bnd_reg.match(record) is not None:
+            record_id = record.split("\t", 3)[2]
+            mate_id_match = mate_id_reg.match(record)
+            if mate_id_match:
+                # if no mate_id is available, this part is skipped
+                # and the record is just piped out normally
+                mate_id = mate_id_match.group(1)
+                if mate_id in bnds:
+                    ret.append(bnds[mate_id])
+                    del bnds[mate_id]
+                else:
+                    bnds[record_id] = record
+                    continue
+        ret.append(record)
+        if (i-1) % n == 0:
+            yield ret
+            ret = []
+    if ret:
+        yield ret
 
-    with VariantFile(filename) as f:
-        pipe.write(str(f.header).encode())
-        for record in f.fetch(*region):
-            pipe.write(str(record).encode())
-    pipe.close()
-    sp.wait()
-    return outfilename
 
-
-def create_regions(filename, stepsize):
-    # smart region geration, large regions with no coverage are ignored
-    with VariantFile(filename) as f:
-        for name, contig in f.header.contigs.items():
-            start = 0
-            while start < contig.length:
-                first_record = next(f.fetch(name, start), None)
-                if not first_record:
-                    break  # no more variants in contig
-                start = first_record.pos
-                stop = min(start + stepsize - 1, contig.length)
-                yield (name, start, stop)
-                start = stop + 1
+def init_worker(function, command, header, inqueue, outqueue):
+    function.header = header
+    function.inqueue = inqueue
+    function.outqueue = outqueue
+    function.command = command
+    function.header = header
 
 
 def execute(filename, command, stepsize, ncores, tmp_dir):
-    # create regions
-    # regions = create_regions(filename, stepsize)
-    regions = list(create_regions_advanced(filename, stepsize))
-    print(regions)
-    exit()
+    # generate header
+    with open(filename) as f:
+        header = []
+        for line in f:
+            if not line.startswith("#"):
+                break
+            header.append(line)
+        header = "".join(header)
 
-    # command = "SnpSift annotate {ss_args} {args.database_vcf} /dev/stdin"
+        bnd_reg = re.compile(".*SVTYPE=BND.*")
+        mate_id_reg = re.compile(".*MATEID=(\w*).*")
+        inqueue = mp.Queue()
+        outqueue = mp.Queue()
 
-    with tempfile.TemporaryDirectory(dir=tmp_dir) as tmp_dir:
-        with Pool(ncores) as p:
-            first = True
-            execute_partial = partial(execute_single, filename, command, tmp_dir)
-            for outfilename in p.imap(execute_partial, regions):
-                # print("merge", outfilename, file=sys.stderr)
-                if first:
-                    cmd = f"cat {outfilename}"
+        init_worker(execute_single, command, header, inqueue, outqueue)
+        worker = []
+
+        # add worker Process
+        for i in range(ncores):
+            print("add worker", file=sys.stderr)
+            p = mp.Process(target=execute_single)
+            p.start()
+            worker.append(p)
+
+        def read():
+            print(header, end="")
+            active_count = ncores
+            while active_count > 0:
+                data = outqueue.get()
+                if not data:
+                    active_count -= 1
                 else:
-                    cmd = f'cat {outfilename} | grep -v "#"'
-                os.system(cmd)
-                os.remove(outfilename)
-                first = False
+                    print(data, end="")
 
+        reader = Thread(target=read)
+        reader.start()
+
+        record_chunks = grouper(stepsize, chain([line], f), bnd_reg, mate_id_reg)
+        for chunk in record_chunks:
+            inqueue.put(chunk)
+
+        for i in range(ncores):
+            inqueue.put(None)
+
+        reader.join()
+        # with Pool(1, initializer=init_worker, initargs=(execute_single, command, header, inqueue, outqueue)) as p:
+        #     list(p.imap(execute_single, records))
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("vcf")
     parser.add_argument("command")
-    parser.add_argument("--stepsize", "-s", type=int, default=1000000)
-    parser.add_argument("--threads", "-t", type=int, default=8)
+    parser.add_argument("vcf")
+    parser.add_argument("--stepsize", "-s", type=int, default=10000)
+    parser.add_argument("--threads", "-t", type=int, default=4)
     parser.add_argument(
         "--tmp", default=None, help="Define the base directory for the tmp files"
     )
